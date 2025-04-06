0f9797cb7e720d006a69ffba98eb12ab4271c354
Oleksandra Ovcharova <oleksaovcharova@gmail.com>
2025-04-06 22:37:11 +0200

    Reviews.

---
+++ src/js/reviews.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import Swiper from 'swiper';
+import { Navigation, Keyboard } from 'swiper/modules';
+import 'swiper/css';
+
+const reviewsList = document.querySelector('ul.reviews-list');
+
+async function getReviews() {
+  try {
+    const response = await axios.get(
+      'https://portfolio-js.b.goit.study/api/reviews'
+    );
+    return response.data;
+  } catch {}
+}
+
+function renderReviews(reviews) {
+  const markup = reviews
+    .map(({ id, author, avatar_url, review }) => {
+      return `<li class="reviews-list-item swiper-slide" id="review-${id}">
+        <img src="${avatar_url}" class="review-img" />
+        <p class="review-name">${author}</p>
+        <p class="review-text">${review}</p>
+      </li>`;
+    })
+    .join('');
+
+  reviewsList.insertAdjacentHTML('beforeend', markup);
+}
+
+const reviews = await getReviews();
+renderReviews(reviews);
+
+new Swiper('.reviews-swiper', {
+  modules: [Navigation, Keyboard],
+  slidesPerView: 1,
+  spaceBetween: 16,
+  autoHeight: true,
+  navigation: {
+    disabledClass: 'disabled',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1440: {
+      slidesPerView: 4,
+    },
+  },
+});
